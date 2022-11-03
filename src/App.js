729d45e5803c84efd31ed1613dd00c2246ab9098
unknown <gitusertester@gmail.com>
2022-11-03 13:05:56 +0545

    routing with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+import './App.css'
+import About from './pages/About'
+import Article from './pages/Article'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          {/* <Route path='articles/:id' component={Article} /> */}
+          <Route path='articles/:id' element={<Article />} />
+          {/* <Route path='*' element={<Navigate replace to='/' />}></Route> */}
+          <Route path='*' element={<Navigate to='/' />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
